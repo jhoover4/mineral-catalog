a336a1550f828fcb5760509b5ea917fe6914ea1c
jhoover4 <jordan@hoovermld.com>
2018-09-06 17:56:09 -0500

    Updated filters.

--- mineral_catalog/views.py
+++ mineral_catalog/views.py
@@ -5,7 +5,7 @@ from django.core.paginator import Paginator
 from django.db.models import Q
 from django.shortcuts import render
 
-from mineral_detail.models import Mineral, Group
+from mineral_detail.models import Mineral, Group, Category
 
 
 def index(request):
@@ -13,22 +13,22 @@ def index(request):
     Index view is used for showing and filtering minerals. Search box takes precedence over letter filtering.
     """
 
-    filters = {}
-
     search_box_query = request.GET.get('search-box', None)
     group_search_query = request.GET.get('group', None)
-    color_search_query = request.GET.get('color', None)
-    letter_search_query = None
-    search_term = None
+    category_search_query = request.GET.get('cat', None)
+    searched_letter = None
 
     if search_box_query:
-        filters['search_box_query'] = Q(name__icontains=search_box_query)
-    if group_search_query:
-        filters['group_search_query'] = Q(group__name=group_search_query)
-    if color_search_query:
-        filters['color_search_query'] = Q(color=color_search_query)
+        query = Q(name__icontains=search_box_query)
+    elif group_search_query:
+        query = Q(group__name=group_search_query)
+    elif category_search_query:
+        query = Q(category__name=category_search_query)
+    else:
+        searched_letter = request.GET.get('letter', 'a')
+        query = Q(name__startswith=searched_letter)
 
-    filtered_minerals = Mineral.objects.filter(**filters)
+    filtered_minerals = Mineral.objects.filter(query)
 
     all_mineral_ids = Mineral.objects.all().values('id')
     rand_num = random.randint(1, all_mineral_ids.count())
@@ -38,14 +38,16 @@ def index(request):
     minerals = paginator.get_page(page)
 
     groups = Group.objects.values('name').order_by('name')
+    categories = Category.objects.values('name').order_by('name')
 
     context = {
         'minerals': minerals,
         'rand_mineral': rand_num,
         'letters': string.ascii_lowercase,
-        'searched_letter': letter_search_query,
-        'search_term': search_term,
-        'mineral_groups': groups
+        'searched_letter': searched_letter,
+        'search_term': search_box_query,
+        'mineral_groups': groups,
+        'mineral_categories': categories,
     }
 
     return render(request, 'index.html', context)
